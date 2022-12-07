c1fc4f29fbe89f888f366dc7db3cafdf75bccff5
jwchae8 <jwchae8@gmail.com>
2022-12-07 13:10:31 +0900

    address review

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -7,6 +7,9 @@
 
 (comment
   (sum [1 2 3]))
+;; apply 함수 사용 시도해보기
+(comment (
+           apply + [1 2 3]))
 
 ;; 파트 2
 ;; 주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
@@ -20,3 +23,12 @@
           x))
 
 (comment (find-first-repeated-occurrence (reductions + (cycle [3 3 4 -2 -4]))))
+
+;; 데이터를 가공하는 함수를 별도로 만들거나 let을 이용해보기 데이터와 로직 분리하기
+;; threading macro 사용하기
+(max [1234])
+(->> [3 3 4 -2 -4]
+     cycle
+     (reductions +)
+     find-first-repeated-occurrence
+     ,)
