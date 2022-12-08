98440e5878bc11a93e08038261b1e6e534879319
jwchae8 <jwchae8@gmail.com>
2022-12-08 16:54:12 +0900

    add comments

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -24,6 +24,9 @@
 ;; 좀 더 명확하게 결과값을 알고 싶으면 contains?를 쓰는 것이 좋음
 ;(defn has-value-n [n]
 ;  (fn [coll] (coll n)))
+
+;; input: set에 포함되어 있는지 검사하고자 하는 원소
+;; output: 원소 n이 포함된 set들을 얻을 수 있는 filter partial function
 (defn filter-value-n [n]
   (partial filter (fn [count-set] (count-set n))))
 
@@ -31,23 +34,33 @@
 ;(defn has-value-2-3 [coll]
 ;  ((juxt (has-value-n 2) (has-value-n 3)) coll))
 
+;; input: 검사 대상이 되는 set
+;; output: 두 값이 들어있는 벡터로, 첫 값은 set에 2가 들어있으면 2, 없으면 nil
+;;         두 번째 값은 3이 들어있으면 3, 없으면 nil
 (defn has-value-2-3 [count-set]
   ((juxt #(% 2) #(% 3)) count-set))
+(has-value-2-3 #{ 1 2 3})
 
 (defn accumulate-non-nil [idx]
   (fn [xs] (reduce (fn [x y] (if (nil? (nth y idx))
                                x
                                (+ 1 x))) 0 xs)))
 
+;; input: 검사 대상이 되는 set의 벡터
+;; output: 두 벡터가 들어있는 벡터로 첫 벡터는 input으로 들어온 set들 중 2가 포함된 것만 있음
+;;         두 번째 벡터는 input으로 들어온 set들 중 3이 포함된 것만 있음
 (defn filter-value-2-3 [xs]
   ((juxt (filter-value-n 2) (filter-value-n 3)) xs))
 
 ;; mapv는 eager한 함수(cycle과 함께 쓰면 무한 루프에 빠지게 됨)
+;; input: 벡터의 벡터(행렬)
+;; output: input이 transpose된 결과의 벡터의 벡터
 (defn transpose [m]
   (apply mapv vector m))
 
 
-
+;; input: 문자열
+;; output: key는 문자이고 value는 갯수인 hashmap
 (defn counters-set [text] (->
                              text
                              frequencies
@@ -103,12 +116,17 @@
               slurp
               clojure.string/split-lines))
 (def indexed-input2 (map-indexed vector input2))
+
+;; input: 두 문자열
+;; output: 두 문자열 각 자리를 비교했을 때 차이나는 문자의 갯수
 (defn str-diff [str1 str2]
   (->>
     (map vector str1 str2)
     (filter #(apply not= %))
     count))
 (str-diff "asdf" "assf")
+;; input: 두 문자열
+;; output: 두 문자열 각 자리가 동일한 것만 모아서 나온 문자열
 (defn find-common [str1 str2]
   (->>
     (map (fn [x y] (if (= x y)
