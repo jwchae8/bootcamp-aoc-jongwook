27dcfcac3de57f39e3e7ef675e2e9e2b1ddac14f
jwchae8 <jwchae8@gmail.com>
2022-12-09 09:55:24 +0900

    address review

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -86,7 +86,7 @@
 
 ;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
 ;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
-
+;; concat으로 한 벡터를 만든 후 거기서 frequency를 구해보기
 (comment (->>
            input-lines
            (map parse-input)
@@ -104,6 +104,8 @@
 ;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
 ;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
 
+;; reduce대신 filter 사용해보기
+;; 라이브러리 filter-vals
 (comment (->>
            input-lines2
            (map parse-input)
@@ -113,6 +115,6 @@
            vals
            (map build-id-to-count-hashmap)
            (reduce (fn [x y] (merge-with max x y)))
-           (reduce (fn [x y] (if (= (val y) 1)
-                               (reduced (key y))
-                               y)))))
+           (filter #(= (val %) 1))
+           first
+           key))
