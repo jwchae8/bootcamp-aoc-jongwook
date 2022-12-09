b54a3668b86e37c420ec5ddb13c51e16dc4b73c3
jwchae8 <jwchae8@gmail.com>
2022-12-09 16:28:03 +0900

    solve day4

--- clojure/src/aoc2018_4.clj
+++ clojure/src/aoc2018_4.clj
@@ -1,4 +1,82 @@
 (ns aoc2018_4)
+
+;; example
+;; [1518-07-04 00:01] falls asleep
+;; [1518-06-27 00:42] wakes up
+;; [1518-06-07 00:03] Guard #2789 begins shift
+(def input-lines (->
+                   "resources/day4"
+                   slurp
+                   clojure.string/split-lines))
+
+;; 파싱을 위한 정규표현식
+;; 분(minute)값과 가드의 ID만을 구한다
+(def input-pattern #":(\d+)[^0-9]*(\d+)?")
+
+;; nil이면 nil, 아니면 parse-long 결과를 반환
+;; 이게 idiomatic한 접근인지는 잘 모르겠습니다만...
+;; example
+;; "1234" -> 1234
+;; nil -> nil
+(defn parse-long-or-nil [string] (if (nil? string)
+                                   nil
+                                   (parse-long string)))
+
+
+(defn parse-log [log] (->>
+                        log
+                        (re-find input-pattern)
+                        rest
+                        (map parse-long-or-nil)
+                        (zipmap [:minute :guard-id])))
+
+;; input: {:minute 분값 :guard-id 가드ID 혹은 nil값}으로 이뤄진 시퀀스
+;; output: {:logs 한 가드가 자거나 깬 분값의 시퀀스 :guard-id 가드ID}로 이뤄진 시퀀스
+;; example
+;;
+(defn group-logs-by-guard-id [logs] (->>
+                                      logs
+                                      (partition-by (fn [log] (nil? (:guard-id log))))
+                                      (partition 2)
+                                      (map (fn [[guard minutes]]
+                                             {(:guard-id (last guard)) (map :minute minutes)}))
+                                      (apply merge-with concat)
+                                      (map (fn [[guard minutes]]
+                                             {:guard-id guard :logs (map (fn [interval] (zipmap [:sleep :wake] interval)) (partition 2 minutes))}))))
+
+(->>
+  input-lines
+  sort
+  (map parse-log)
+  (partition-by (fn [log] (nil? (:guard-id log))))
+  (partition 2))
+
+(defn sleep-time [{:keys [logs]}] (->>
+                                   logs
+                                   (map (fn [{:keys [wake sleep]}] (- wake sleep)))
+                                   (apply +)))
+
+(defn max-sleep-time-guard [log-by-guards] (->>
+                                             log-by-guards
+                                             (apply max-key sleep-time)))
+
+(defn most-sleeping-minute [{:keys [guard-id logs]}] (->>
+                                                       logs
+                                                       (map #(range (:sleep %) (:wake %)))
+                                                       (reduce concat)
+                                                       frequencies
+                                                       (apply max-key val)
+                                                       (#(hash-map :guard-id guard-id
+                                                                   :minute (key %)
+                                                                   :frequency (val %)))))
+
+
+(defn answer [{:keys [guard-id minute]}] (* guard-id minute))
+
+
+
+
+
 ;; 파트 1
 ;; 입력:
 
@@ -28,6 +106,23 @@
 ;; 파트 1은 “주어진 입력에 대해서, 가장 오랜시간 잠들어있었던 가드의 ID와, 그 가드가 가장 빈번하게 잠들어 있었던 분(minute)의 곱을 구하라”
 ;; 만약 20번 가드가 0시 10분~36분, 다음날 0시 5분~11분, 다다음날 0시 11분~13분 이렇게 잠들어 있었다면, “11분“이 가장 빈번하게 잠들어 있던 ‘분’. 그럼 답은 20 * 11 = 220.
 
+(->>
+  input-lines
+  sort
+  (map parse-log)
+  group-logs-by-guard-id
+  max-sleep-time-guard
+  most-sleeping-minute
+  answer)
 
 ;; 파트 2
 ;; 주어진 분(minute)에 가장 많이 잠들어 있던 가드의 ID과 그 분(minute)을 곱한 값을 구하라.
+
+(->>
+  input-lines
+  sort
+  (map parse-log)
+  group-logs-by-guard-id
+  (map most-sleeping-minute)
+  (apply max-key :frequency)
+  answer)
